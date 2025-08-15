90cee831faa76b51ef43f1cb74196d884a167457
Rahul Tiwari <128848653+rrtiwari@users.noreply.github.com>
2025-08-15 17:54:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+    let a=[...arr].sort((a,b)=>a-b);
+	let n=a.length;
+	let cost=0;
+	while(n>1){
+		let first=a.shift;
+		let second=b.shift;
+		let newrope=first+second;
+		let cost+=newrope;
+		a.push(newrope);
+		a.sort((a,b)=>a-b);
+	}
+	return cost;
 }
-
 module.exports=mincost;
